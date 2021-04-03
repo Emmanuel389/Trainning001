9a0c39d72da87185153cd70658be369a0d9109a1
Emmanuel389 <emmanuel.nietomu@gmail.com>
2021-04-03 18:56:58 -0300

    Nombre del proyecto: Tranning001, Descripción: versión inicial del pryecto

---
+++ src/main/java/com/talento/springboot/webapp/tranning001/Tranning001Application.java
@@ -0,0 +1,13 @@
+package com.talento.springboot.webapp.tranning001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tranning001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tranning001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/talento/springboot/webapp/tranning001/Tranning001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talento.springboot.webapp.tranning001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tranning001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
